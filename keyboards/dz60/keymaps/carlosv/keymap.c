8034bb34a8d41c96f48d78ab7a95cbe126359710
cvfcvf <cvfcvf@protonmail.com>
2022-09-04 00:55:05 +0200

    Tapdace ñ/insert

--- keyboards/dz60/keymaps/carlosv/keymap.c
+++ keyboards/dz60/keymaps/carlosv/keymap.c
@@ -3,12 +3,14 @@
 // Tap Dance declarations
 enum {
     TD_ESC_CAPS,
+    TD_ENiE_INS,
 };
 
 // Tap Dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+    [TD_ENiE_INS] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_INS),
 };
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -39,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------'
    */
   [_BASE] = LAYOUT_60_iso(
-    TD(TD_ESC_CAPS), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,          KC_EQL,     KC_BSPC,
-    KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,          KC_RBRC,
-    MO(_NAV2),       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_NUHS,    KC_ENT,
-    KC_LSFT,         KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-    KC_LCTL,         KC_LGUI, KC_LALT,                            LT(_NAV1,KC_SPC),                   KC_RALT, LT(_NAV2,KC_APP), MO(_MEDIA), KC_RCTL),
+    TD(TD_ESC_CAPS), KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS,          KC_EQL,     KC_BSPC,
+    KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC,          KC_RBRC,
+    MO(_NAV2),       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_ENiE_INS), KC_QUOT,          KC_NUHS,    KC_ENT,
+    KC_LSFT,         KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,          KC_SLSH,          KC_RSFT,
+    KC_LCTL,         KC_LGUI, KC_LALT,                            LT(_NAV1,KC_SPC),                   KC_RALT,         LT(_NAV2,KC_APP), MO(_MEDIA), KC_RCTL),
 
 
   [_MEDIA] = LAYOUT_60_iso(
