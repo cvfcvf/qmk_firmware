cee40b17245758b04e0ea12b0deed8d83dd6f97b
cvfcvf <cvfcvf@protonmail.com>
2023-03-08 10:56:25 +0100

    Añadido (tarde) simbolos_ES.h

---
+++ keyboards/converter/usb_usb/keymaps/carlosv/simbolos_ES.h
@@ -0,0 +1,27 @@
+/*
+
+KC_LPRN
+KC_RPRN
+KC_LCBR
+KC_RCBR
+
+*/
+
+#undef KC_LPRN
+#undef KC_RPRN
+
+#undef KC_LCBR
+#undef KC_RCBR
+//#undef KC_ASTR
+
+
+#define KC_LPRN LSFT(KC_8)
+#define KC_RPRN LSFT(KC_9)
+#define KC_LCBR RALT(KC_QUOT)
+#define KC_RCBR RALT(KC_BSLS)
+
+
+#define KC_LBRK RALT(KC_LBRC)
+#define KC_RBRK RALT(KC_RBRC)
+
+//#define KC_ASTR LSFT(KC_RBRC)
