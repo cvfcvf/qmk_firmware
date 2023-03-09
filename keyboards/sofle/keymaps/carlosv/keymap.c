c808c6bdbc7f1fa6fdbc0007cd664631ae794c0b
cvfcvf <cvfcvf@protonmail.com>
2023-03-09 23:37:44 +0100

    Añadido soporte para HRM y macros personales.
    Copia de los del conversor usb-usb.

--- keyboards/sofle/keymaps/carlosv/keymap.c
+++ keyboards/sofle/keymaps/carlosv/keymap.c
@@ -1,5 +1,68 @@
 #include QMK_KEYBOARD_H
 
+#include "keymap_spanish.h"
+// Alias personales
+
+#define _NAV _RAISE
+#define SYM1 _LOWER
+
+//#define CAPS_NUM LT(_NAV2,KC_CAPS)
+#define TABmed LT(_ADJUST,KC_TAB)
+#define TABsym LT(SYM1,KC_TAB)
+#define ESCmed LT(_ADJUST,KC_ESC)
+#define MNUmed LT(_ADJUST,KC_APP)
+#define SPC_NAV LT(_NAV,KC_SPC)
+//#define CAPSgui MT(KC_LGUI,KC_CAPS)
+#define CAPSgui LGUI_T(KC_CAPS)
+#define CAPSsym1 LT(SYM1,KC_CAPS)
+
+#define CTLEFT LCTL(KC_LEFT)
+#define CTRGHT LCTL(KC_RGHT)
+
+// Alias para acortar los códigos de TAP DANCE
+#define LS_CAP TD(TD_LSFT_CAPS)
+#define LC_INS TD(TD_LCTL_INS)
+//#define LS_CAP TD()
+
+
+// Layer shorthand
+/* enum _layer { */
+/*              _BASE, */
+/* 	     _ADJUST, */
+/* 	     _NAV, */
+/* 	     SYM1, */
+/* 	     //_BLANK */
+/* }; */
+
+// Left-hand home row mods
+#define HR_A LGUI_T(KC_A)
+#define HR_S LALT_T(KC_S)
+#define HR_D LCTL_T(KC_D)
+#define HR_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HR_J RSFT_T(KC_J)
+#define HR_K RCTL_T(KC_K)
+#define HR_L LALT_T(KC_L)
+#define HR_SCLN RGUI_T(KC_SCLN)
+
+// Tap Dance declarations
+enum {
+    TD_ESC_CAPS,
+    TD_ENiE_INS,
+    TD_LSFT_CAPS,
+    TD_LCTL_INS,
+};
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+    [TD_ENiE_INS] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_INS),
+    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    [TD_LCTL_INS] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_INS)
+};
+
 enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
@@ -43,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_GRV,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+  KC_TAB,   HR_A,   HR_S,    HR_D,    HR_F,    KC_G,                     KC_H,    HR_J,    HR_K,    HR_L, HR_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                  KC_LGUI,KC_LALT,KC_LCTRL, KC_LOWER, KC_ENT,      KC_SPC,  KC_RAISE, KC_RCTRL, KC_RALT, KC_RGUI
 ),
@@ -84,6 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
+
 [_LOWER] = LAYOUT(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
