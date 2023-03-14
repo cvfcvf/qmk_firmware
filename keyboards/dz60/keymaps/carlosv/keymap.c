de1668390f91d846290eb76c34cfeba1dfc810a0
cvfcvf <cvfcvf@protonmail.com>
2023-03-14 15:54:47 +0100

    Soporte completo para capa QWERTY sin modificadores.

--- keyboards/dz60/keymaps/carlosv/keymap.c
+++ keyboards/dz60/keymaps/carlosv/keymap.c
@@ -15,6 +15,7 @@
 
 #define CTLEFT LCTL(KC_LEFT)
 #define CTRGHT LCTL(KC_RGHT)
+#define CTL_QWY TD(TD_QWERTY)
 
 // Alias para acortar los códigos de TAP DANCE
 #define LS_CAP TD(TD_LSFT_CAPS)
@@ -42,25 +43,9 @@ enum {
     TD_ENiE_INS,
     TD_LSFT_CAPS,
     TD_LCTL_INS,
-    TD_MENU_QWERTY,
+    TD_QWERTY,
     TD_MENU_BASE,
 };
-
-// Tap Dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
-    [TD_ENiE_INS] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_INS),
-    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-    [TD_LCTL_INS] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_INS),
-    /* [TD_MENU_QWERTY] = ACTION_TAP_DANCE_DOUBLE(KC_APP, TO(_QWERTY)), */
-    /* [TD_MENU_BASE] = ACTION_TAP_DANCE_DOUBLE(KC_APP, TO(_BASE)), */
-};
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- * _______ can be used in place of KC_TRNS (transparent) *
- * XXXXXXX can be used in place of KC_NO (No Operation)  *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 // Layer shorthand
 enum _layer {
@@ -71,6 +56,23 @@ enum _layer {
 	     _MEDIA,
 	     _NUM
 };
+
+// Tap Dance definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+    [TD_ENiE_INS] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_INS),
+    [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+    [TD_LCTL_INS] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_INS),
+    [TD_QWERTY] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_RCTL, _QWERTY)
+    /* [TD_MENU_BASE] = ACTION_TAP_DANCE_DOUBLE(KC_APP, TO(_BASE)), */
+};
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * _______ can be used in place of KC_TRNS (transparent) *
+ * XXXXXXX can be used in place of KC_NO (No Operation)  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* ISO 6u layout layer 0 (UK Layout shown)
@@ -91,20 +93,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TABsym,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC,          KC_RBRC,
     CAPS_NUM, HR_A,    HR_S,    HR_D,    HR_F,    KC_G,    KC_H,    HR_J,    HR_K,    HR_L,    HR_SCLN,         KC_QUOT,          KC_NUHS,    KC_ENT,
     LS_CAP ,  KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,          KC_SLSH,          KC_RSFT,
-    LC_INS ,  KC_LGUI, KC_LALT,                            SPC_NAV,                            KC_RALT,         LT(_NUM,KC_APP),  DF(_QWERTY), KC_RCTL),
+    LC_INS ,  KC_LGUI, KC_LALT,                            SPC_NAV,                            KC_RALT,         MO(_MEDIA),LT(_NUM,KC_APP),   CTL_QWY),
 
   [_QWERTY] = LAYOUT_60_iso(
     QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,            KC_MINS,          KC_EQL,     KC_BSPC,
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC,          KC_RBRC,
     KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT,          KC_NUHS,    KC_ENT,
     KC_LSFT,  KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,          KC_SLSH,          KC_RSFT,
-    KC_LCTL,  KC_LGUI, KC_LALT,                            KC_SPC ,                            KC_RALT,         KC_MENU,          DF(_BASE),    KC_RCTL),
+    KC_LCTL,  KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT,         KC_RGUI,          KC_APP,     CTL_QWY),
 
   [_NAV] = LAYOUT_60_iso(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, CTLEFT,  KC_UP,   CTRGHT,  XXXXXXX, XXXXXXX, XXXXXXX,
     _______, _______, _______, _______, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , XXXXXXX, XXXXXXX, XXXXXXX,
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, KC_END , KC_ENT , XXXXXXX,
+    _______, XXXXXXX, KC_UNDO, XXXXXXX, KC_WBAK, KC_WFWD, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, KC_END , KC_ENT , XXXXXXX,
     _______, _______, _______,                            XXXXXXX,                            _______, XXXXXXX, XXXXXXX, _______),
 
   [_SYM] = LAYOUT_60_iso(
