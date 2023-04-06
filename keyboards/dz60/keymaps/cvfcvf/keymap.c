721e90e1a31bb4d911126d710e19046c60bf33a7
cvfcvf <cvfcvf@protonmail.com>
2023-04-07 00:21:51 +0200

    Añadido CAPSLOCK en tecla G capa NAV.

--- keyboards/dz60/keymaps/cvfcvf/keymap.c
+++ keyboards/dz60/keymaps/cvfcvf/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NAV] = LAYOUT_60_iso(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, CTLEFT,  KC_UP,   CTRGHT,  XXXXXXX, XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , XXXXXXX, XXXXXXX, XXXXXXX,
+    _______, _______, _______, _______, _______, KC_CAPS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , XXXXXXX, XXXXXXX, XXXXXXX,
     _______, XXXXXXX, KC_UNDO, XXXXXXX, KC_WBAK, KC_WFWD, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, KC_END , KC_ENT , XXXXXXX,
     _______, _______, _______,                            XXXXXXX,                            _______, XXXXXXX, XXXXXXX, _______),
 
