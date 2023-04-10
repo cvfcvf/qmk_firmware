5f3f6f2c315a978f8aa4e91a67f865bd198339a9
cvfcvf <cvfcvf@protonmail.com>
2023-04-10 18:01:51 +0200

    Comentada toda la configuración
    Habría que eliminar este fichero. Se deja por si se necesita alguna
    configuración posterior

--- keyboards/durgod/k320/keymaps/cvfcvf/config.h
+++ keyboards/durgod/k320/keymaps/cvfcvf/config.h
@@ -15,35 +15,35 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#pragma once
+/* #pragma once */
 
-//#include "config_common.h"
+/* //#include "config_common.h" */
 
-/* USB Device descriptor parameter */
-#define PRODUCT_ID      0x3200
-// Taurus K320 models:
-//#define DEVICE_VER      0x0001 // Base (no backlight)
-//#define DEVICE_VER      0x0002 // Corona (white led matrix)
-//#define DEVICE_VER      0x0003 // Aurora (red & blue led matrix)
-//#define DEVICE_VER      0x0004 // Nebula (rgb led matrix)
+/* /\* USB Device descriptor parameter *\/ */
+/* #define PRODUCT_ID      0x3200 */
+/* // Taurus K320 models: */
+/* //#define DEVICE_VER      0x0001 // Base (no backlight) */
+/* //#define DEVICE_VER      0x0002 // Corona (white led matrix) */
+/* //#define DEVICE_VER      0x0003 // Aurora (red & blue led matrix) */
+/* //#define DEVICE_VER      0x0004 // Nebula (rgb led matrix) */
 
-/* key matrix size (cols in generic keyboard config) */
-#define MATRIX_ROWS 7
+/* /\* key matrix size (cols in generic keyboard config) *\/ */
+/* #define MATRIX_ROWS 7 */
 
-#define MATRIX_ROW_PINS { A0, A1, A2, A3, A4, A5, A6 }
+/* #define MATRIX_ROW_PINS { A0, A1, A2, A3, A4, A5, A6 } */
 
 /* Configuración de space cadet */
-#define LSPO_KEY KC_8
-#define RSPC_KEY KC_9
+/* #define LSPO_KEY KC_8 */
+/* #define RSPC_KEY KC_9 */
 
 /* Configuración de Home Row Mods */
 
-// Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 250
+/* // Configure the global tapping term (default: 200ms) */
+/* #define TAPPING_TERM 250 */
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+/* // Prevent normal rollover on alphas from accidentally triggering mods. */
+/* #define IGNORE_MOD_TAP_INTERRUPT */
 
-// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define TAPPING_FORCE_HOLD
+/* // Enable rapid switch from tap to hold, disables double tap hold auto-repeat. */
+/* #define TAPPING_FORCE_HOLD */
 
