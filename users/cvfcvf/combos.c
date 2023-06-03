94a8be2ecb8909b48054a376b698ebedea29dc28
cvfcvf <cvfcvf@protonmail.com>
2023-06-03 15:25:44 +0200

    Desactiva los combos en conf general y los activa en el keymap.c

--- users/cvfcvf/combos.c
+++ users/cvfcvf/combos.c
@@ -1,10 +1,10 @@
 #include "keymap_spanish.h"
 #include "cvfcvf.h"
 
-// Combos
-uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
-const uint16_t PROGMEM fj_combo[] = {HR_F, HR_J, COMBO_END};
+/* // Combos */
+/* uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead! */
+/* const uint16_t PROGMEM fj_combo[] = {HR_F, HR_J, COMBO_END}; */
 
-combo_t key_combos[] = {
-  [FJ_TILDE] = COMBO(fj_combo, ES_ACUT),
-  };
+/* combo_t key_combos[] = { */
+/*   [FJ_TILDE] = COMBO(fj_combo, ES_ACUT), */
+/*   }; */
