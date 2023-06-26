65520ab8ba1b870ae30ea6c20bac7d63646d4e27
cvfcvf <cvfcvf@protonmail.com>
2023-06-26 12:05:42 +0200

    Triestado y soporte CAPS_WORD

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/cvfcvf/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/cvfcvf/rules.mk
@@ -6,12 +6,15 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no           # Enable N-Key Rollover
+NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 TAP_DANCE_ENABLE = yes      # Soporte para TAP DANCE
 COMBO_ENABLE = yes          # Combos
+CAPS_WORD_ENABLE = yes
 
 EXTRAFLAGS += -flto
 SRC += cvfcvf.c
+
+# RGB_MATRIX_ENABLE = no
