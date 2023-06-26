3f1bc237b4665b49d3c99e1796774a1580a4d96c
cvfcvf <cvfcvf@protonmail.com>
2023-06-26 12:03:19 +0200

    Ajuste de keymap al sistema de capas.

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/cvfcvf/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/cvfcvf/keymap.c
@@ -51,11 +51,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | BLTog| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
+//[_QWRTY] = LAYOUT_ortho_4x12_1x2uC(
 [_BASE] = LAYOUT_ortho_4x12_1x2uC(
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  HR_A,    HR_S,    HR_D,    HR_F,    KC_G,    KC_H,    HR_J,    HR_K,    HR_L,    HR_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, TL_LOWR, SPC_NAV, TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  HR_A,    HR_S,    HR_D,    HR_F,    KC_G,    KC_H,    HR_J,    HR_K,    HR_L,    HR_SCLN, KC_QUOT,
+    KC_LSFT, em_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MINSemacs, KC_ENT ,
+    KC_LCTL, RGB_TOG, KC_LGUI, KC_LALT, TL_LOWR,      SPC_NAV,     TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
+
+[_NAV] = LAYOUT_ortho_4x12_1x2uC(
+    KC_GRV , XXXXXXX, XXXXXXX, KC_APP , XXXXXXX, KC_INS , KC_PGUP, CTLEFT,  KC_UP,   CTRGHT,  XXXXXXX, XXXXXXX,
+    _______, _______, _______, _______, _______, CW_TOGG, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , XXXXXXX,
+    _______, KC_UNDO, XXXXXXX, KC_WBAK, KC_WFWD, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, KC_END , XXXXXXX, KC_ENT ,
+    KC_LCTL, RGB_TOG, KC_LGUI, KC_LALT, TL_LOWR,      SPC_NAV,     KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -69,18 +77,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_LOWER] = LAYOUT_ortho_4x12_1x2uC(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
-),
-
-[_NAV] = LAYOUT_ortho_4x12_1x2uC(
-    _______, XXXXXXX, XXXXXXX, KC_APP , XXXXXXX, KC_INS , KC_PGUP, CTLEFT,  KC_UP,   CTRGHT,  XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______, _______, KC_CAPS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , XXXXXXX,
-    _______, KC_UNDO, XXXXXXX, KC_WBAK, KC_WFWD, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, KC_END , XXXXXXX, KC_ENT ,
-    RGB_TOG, KC_LCTL, KC_LALT, KC_LGUI, TL_LOWR, SPC_NAV, TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+//[_LOWER] = LAYOUT_ortho_4x12_1x2uC(
+[_SYM] = LAYOUT_ortho_4x12_1x2uC(
+    _______, ES_EXLM, ES_DQUO, ES_BULT, ES_DLR,  ES_PERC, ES_AMPR, ES_IEXL,    ES_LBRC,    ES_RBRC, ES_EXLM, KC_BSPC,
+    KC_DEL,  ES_EQL,  ES_PLUS, ES_ASTR, ES_SLSH, ES_CIRC, ES_BSLS, ES_IQUE,    ES_LPRN,    ES_RPRN, ES_QUES, ES_CCED,
+    _______, miEURO , ES_LABK, ES_RABK, ES_PIPE, ES_QUOT, XXXXXXX, ES_HASH,    ES_LCBR,    ES_RCBR, ES_AT  , _______,
+    _______, _______, _______, _______, _______,      _______,     _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
@@ -94,13 +96,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
-[_RAISE] = LAYOUT_ortho_4x12_1x2uC(
+//[_RAISE] = LAYOUT_ortho_4x12_1x2uC(
+[_NUM] = LAYOUT_ortho_4x12_1x2uC(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______, _______, _______, _______,      _______,     _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
+[_EMACS] = LAYOUT_ortho_4x12_1x2uC(
+/*    ESC  ,    Q   ,    W   ,    E   ,    R   ,    T   ,    Y   ,    U   ,    I   ,    O   ,    P   ,    BS    */
+    XXXXXXX, XXXXXXX, SAVE_AS, XXXXXXX, RECENT , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ORGPDF , XXXXXXX, 
+    XXXXXXX, XXXXXXX, SAVE   , XXXXXXX, OPEN   , MAGIT  , XXXXXXX, XXXXXXX, KILL   , XXXXXXX, XXXXXXX, XXXXXXX, 
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, BUFFER , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+),
 /* Adjust (Lower + Raise)
  *                      v------------------------RGB CONTROL--------------------v
  * ,-----------------------------------------------------------------------------------.
@@ -113,7 +123,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
-[_ADJUST] = LAYOUT_ortho_4x12_1x2uC(
+//[_ADJUST] = LAYOUT_ortho_4x12_1x2uC(
+[_MEDIA] = LAYOUT_ortho_4x12_1x2uC(
     _______, QK_BOOT, DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______,  _______,  _______, _______, _______,
