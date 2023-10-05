2a64f0a9aab42cdffd3093cec28bd2dd9b528f43
Carlos Villagrá <carlosv@ietcc.csic.es>
2023-10-05 13:55:53 +0200

    Transforma capa _NUM en numpad.
    Convierte la capa de números en una capa base, con un teclado
    numérico. Incluye los cambios necesarios para hacer los cambios de
    capa de ida y vuelta.
    
    De momento, se asigna el cambio de capa a la tecla suelta que se usaba
    para encender y apagar el RGB, entre LCTL y LGUI.

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/cvfcvf/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/cvfcvf/keymap.c
@@ -58,14 +58,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  HR_A,    HR_S,    HR_D,    HR_F,    KC_G,    KC_H,    HR_J,    HR_K,    HR_L,    HR_SCLN, KC_QUOT,
     KC_LSFT, em_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MINSemacs, KC_ENT ,
-    KC_LCTL, RGB_TOG, KC_LGUI, KC_LALT, TL_LOWR,      SPC_NAV,     TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, DF(_NUM),KC_LGUI, KC_LALT, TL_LOWR,      SPC_NAV,     TL_UPPR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_NAV] = LAYOUT_ortho_4x12_1x2uC(
     KC_GRV , XXXXXXX, XXXXXXX, KC_APP , XXXXXXX, KC_INS , KC_PGUP, CTLEFT,  KC_UP,   CTRGHT,  XXXXXXX, XXXXXXX,
     _______, _______, _______, _______, _______, CW_TOGG, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , XXXXXXX,
     _______, KC_UNDO, XXXXXXX, KC_WBAK, KC_WFWD, XXXXXXX, XXXXXXX, KC_HOME, XXXXXXX, KC_END , XXXXXXX, KC_ENT ,
-    KC_LCTL, RGB_TOG, KC_LGUI, KC_LALT, TL_LOWR,      SPC_NAV,     KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL, DF(_NUM),KC_LGUI, KC_LALT, TL_LOWR,      SPC_NAV,     KC_RALT, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Lower
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, ES_EXLM, ES_DQUO, ES_BULT, ES_DLR,  ES_PERC, ES_AMPR, ES_IEXL,    ES_LBRC,    ES_RBRC, ES_EXLM, KC_BSPC,
     KC_DEL,  ES_EQL,  ES_PLUS, ES_ASTR, ES_SLSH, ES_CIRC, ES_BSLS, ES_IQUE,    ES_LPRN,    ES_RPRN, ES_QUES, ES_CCED,
     _______, miEURO , ES_LABK, ES_RABK, ES_PIPE, ES_QUOT, XXXXXXX, ES_HASH,    ES_LCBR,    ES_RCBR, ES_AT  , _______,
-    _______, _______, _______, _______, _______,      _______,     _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    _______, _______ ,_______, _______, _______,      _______,     _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
@@ -101,9 +101,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //[_RAISE] = LAYOUT_ortho_4x12_1x2uC(
 [_NUM] = LAYOUT_ortho_4x12_1x2uC(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
-    _______, _______, _______, _______, _______,      _______,     _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_4   , KC_5,    KC_6   , ES_MINS, KC_BSLS,
+    KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_1   , KC_2,    KC_3   , ES_PLUS, KC_ENT ,
+    KC_LCTL,DF(_BASE),KC_LGUI, KC_LALT, TL_LOWR,      SPC_NAV,     _______, KC_0,    KC_DOT , KC_VOLU, KC_MPLY
 ),
 
 [_EMACS] = LAYOUT_ortho_4x12_1x2uC(
