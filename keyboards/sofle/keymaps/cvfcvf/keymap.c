882a61ae11591ee6c7e2adecd91aa4faca704019
cvfcvf <cvfcvf@protonmail.com>
2024-08-04 00:05:45 +0200

    Elimina la tecla de espacio adicional en capa de números.
    Esta tecla tiene que estar asignada al cambio de capas.

--- keyboards/sofle/keymaps/cvfcvf/keymap.c
+++ keyboards/sofle/keymaps/cvfcvf/keymap.c
@@ -229,7 +229,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_CIRC, KC_P7,  KC_P8,   KC_P9,   KC_ASTR, XXXXXXX,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   KC_MINS, KC_P4,  KC_P5,   KC_P6,   KC_EQL,  KC_PIPE,
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,   _______,KC_PLUS, KC_P1,  KC_P2,   KC_P3,   KC_SLSH, _______,
-              _______, OSM(MOD_MEH), _______, _______, _______,   _______,KC_SPC ,  KC_P0,   KC_PDOT, _______
+              _______, OSM(MOD_MEH), _______, _______, _______,   _______,_______,  KC_P0,   KC_PDOT, _______
 ),
 
 /* SWITCH
