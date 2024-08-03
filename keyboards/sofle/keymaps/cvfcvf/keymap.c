d9b247580985ce3f0070a8bcb823c8e1c210c4c3
cvfcvf <cvfcvf@protonmail.com>
2024-08-04 00:02:47 +0200

    Escape en tecla superior derecha. Reconfiguración del MUTE

--- keyboards/sofle/keymaps/cvfcvf/keymap.c
+++ keyboards/sofle/keymaps/cvfcvf/keymap.c
@@ -91,10 +91,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
   [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6 , KC_7,   KC_8,    KC_9,    KC_0,    KC_GRV,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                        KC_6 , KC_7,   KC_8,    KC_9,    KC_0,    KC_ESC,
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC,
   TABGUI ,  KC_A,   HR_S,    HR_D,    HR_F,    KC_G,                      KC_H,    HR_J,   HR_K,    HR_L,    KC_SCLN, KC_QUOT,
-  LS_CAP ,  em_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,  KC_D_MUTE,KC_N,    KC_M,   KC_COMM, KC_DOT,  MINSemacs,KC_ENT ,
+  LS_CAP ,  em_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,  KC_MUTE,  KC_N,    KC_M,   KC_COMM, KC_DOT,  MINSemacs,KC_ENT ,
                  KC_LCTL, KC_LGUI, KC_LALT, TL_LOWR,  TL_UPPR,  KC_BSPC,SPC_NAV ,  KC_RALT, KC_APP, KC_RCTL
 ),
 
